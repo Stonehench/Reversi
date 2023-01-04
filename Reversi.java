86da20ebaf2d4cba81eb5ceb096ddf36cce51b59
Niko2022 <102536857+Niko2022@users.noreply.github.com>
2023-01-04 12:10:43 +0100

    Updated Reversi_V2 m. kommentar på alles koder

--- Reversi.java
+++ Reversi.java
@@ -1,3 +1,6 @@
+package januarProjektet2023;
+
+import java.io.File;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
@@ -5,6 +8,8 @@ import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
@@ -26,22 +31,41 @@ public class Reversi extends Application {
 	int turn;
 
 	@Override
-	public void start(Stage stage) {
+	public void start(Stage primaryStage) {
 
 		// Constructs pane
 		GridPane root = new GridPane();
+		root.setStyle("-fx-background-color: #33CC66;"); // Sets backgground color
 
 		// Size of Window
-		Scene scene = new Scene(root, windowSize, windowSize);
-		stage.setScene(scene);
-		stage.show();
-		stage.setTitle("Reversi");
+		Scene scene = new Scene(root, windowSize + (windowSize / 4), windowSize);
+
+		// Makes CSS and sidepane possible
+		scene.getStylesheets().add(getClass().getResource("test.css").toExternalForm());
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		primaryStage.setTitle("Reversi");
+
+		// Restart button
+		Button restart = new Button("pass");
+		restart.setPrefSize((windowSize / 4), butSize); // Size of the button
+		restart.setStyle("-fx-base: white"); // Button color
+		restart.setStyle("-fx-background-radius: 15"); // Gives button smooth edges
+
+		root.add(restart, gridSize, 6);
+
+		// Pass button
+		Button pass = new Button("Restart game");
+		pass.setPrefSize((windowSize / 4), butSize); // Size of the button
+		pass.setStyle("-fx-base: white"); // Button color
+		pass.setStyle("-fx-background-radius: 15"); // Gives button smooth edges
+
+		root.add(pass, gridSize, 7);
 
 		// Construction the 8x8 Grid with 64 buttons
 		for (int row = 0; row < gridSize; row++) {
 			for (int column = 0; column < gridSize; column++) {
 				MyButton myButton = new MyButton(0);
-				myButton.setStyle("-fx-focus-color: transparent");
 				myButton.setPrefSize(butSize, butSize); // Size of one cell
 				if (column % 2 == 0 && row % 2 == 0) {
 					myButton.setStyle("-fx-base: #8B4513");
@@ -54,15 +78,13 @@ public class Reversi extends Application {
 
 				root.add(myButton, row, column);
 				buttons2D[row][column] = myButton; // Add coordinates and accessibility to all buttons.
-
 			}
 
 		}
 
-		//////////////////////////////////// On-click
-		//////////////////////////////////// function///////////////////////////////////////////
+		//////////////////////////////////// On-click function///////////////////////////////////////////
 
-		// Adds the same function for all buttons on gridPane
+		// Adds all the functionality for when a button is clicked
 		for (int x = 0; x < gridSize; x++) {
 			for (int y = 0; y < gridSize; y++) {
 				buttons2D[x][y].setOnAction(new EventHandler<ActionEvent>() {
@@ -85,13 +107,25 @@ public class Reversi extends Application {
 						if (legal(buttons2D, x, y) == true) {
 							placePiece(buttons2D[x][y]);
 							clickCount++;
-						}
 
+							// SoundFX every time you make a "Legal" move.
+							Media media = new Media(new File("Reversi-game-sound.wav").toURI().toString());
+							MediaPlayer mediaPlayer = new MediaPlayer(media);
+							mediaPlayer.play();
+						} else {
+							// SoundFX every time you make a "Illegal" move.
+							Media media = new Media(new File("emotinal-damage.mp3").toURI().toString());
+							MediaPlayer mediaPlayer = new MediaPlayer(media);
+							mediaPlayer.play();
+							
+						}
 					} // End click function
 				}); // End Event-handler
 			} // Inner loop end
 		} // Outer loop end
 
+		
+		//Constructs the default 4 pieces in the center of Board
 		clickCount++;
 		placePiece(buttons2D[(gridSize / 2) - 1][(gridSize / 2) - 1]);
 		clickCount++;
@@ -102,10 +136,14 @@ public class Reversi extends Application {
 		placePiece(buttons2D[(gridSize / 2) - 1][(gridSize / 2)]);
 		clickCount++;
 
+		//SoundFX: Intro-music when open game
+		Media media = new Media(new File("Intro-Reversi.wav").toURI().toString());
+		MediaPlayer mediaPlayer = new MediaPlayer(media);
+		mediaPlayer.play();
+
 	} // End stage
 
-	////////////////////////////// Action1: Places a piece on the board
-	////////////////////////////////////////////////////////////////////////
+	////////////////////////////// Action1: Places a piece on the board ////////////////////////////////////////////////////////
 	public static void placePiece(MyButton clickedButton) {
 
 		// Design of the WHITE piece
@@ -117,7 +155,7 @@ public class Reversi extends Application {
 		Circle blackPiece = new Circle(butSize / 3 - 2);
 		blackPiece.setFill(Color.BLACK);
 
-		// Players turn: White starts
+		// Players turn: White always starts, then Blacks turn. Repeat.
 		if (clickCount % 2 != 0) {
 			clickedButton.setGraphic(whitePiece); // places White piece on-board
 			playerTurn = "white";
@@ -130,35 +168,10 @@ public class Reversi extends Application {
 		}
 	}
 
-	public static void placePiece(MyButton[][] cell, int x, int y, MyButton clickedButton) {
 
-		// Design of the WHITE piece
-		Circle whitePiece = new Circle(butSize / 3 - 1);
-		whitePiece.setFill(Color.WHITE);
-		whitePiece.setStroke(Color.BLACK);
+	//////////////////////////////////// Peters kode //////////// ///////////////////////////////////////////
 
-		// Design of the BLACK piece
-		Circle blackPiece = new Circle(butSize / 3 - 2);
-		blackPiece.setFill(Color.BLACK);
-
-		// Players turn: White starts
-		if (clickCount % 2 != 0) {
-			clickedButton.setGraphic(whitePiece); // places White piece on-board
-			playerTurn = "white";
-			cell[x][y].setMyValue(1);
-
-		} else {
-			clickedButton.setGraphic(blackPiece); // places Black piece on-board
-			playerTurn = "black";
-			cell[x][y].setMyValue(-1);
-		}
-	}
-
-	//////////////////////////////////// Peters kode
-	//////////////////////////////////// ///////////////////////////////////////////
-
-	// Tjekker om feltet er tomt, går kun videre hvis der ikke allerede er en værdi
-	// tilknyttet feltet
+	// Checks if every move is "Legal" else "Illegal" according to "Rules of Reversi"
 	public boolean legal(MyButton[][] cell, int x, int y) {
 
 		if (cell[x][y].getMyValue() == 0) {
@@ -168,40 +181,25 @@ public class Reversi extends Application {
 		}
 	}
 
-	// Tjekker om der en brik af modsat farve rundt om det valgte felt
+	// Checks if there are enemy pieces around the placed-Piece 
 	public boolean omringet(MyButton[][] cell, int x, int y) {
 
-		// Først sikres at der ikke kan gåes uden for arrayets størrelse for ikke at få
-		// fejl.
+		//Coordinates N/W/E/S of the piece placed
 		int right = x + 1;
 		int left = x - 1;
-
 		int down = y + 1;
 		int up = y - 1;
 
 		// Serves as a border so there wont be an OutOfBounds error
-		if (clickCount % 2 != 0) {
-			turn = 1;
+		if (clickCount % 2 != 0) {turn = 1;} 
+		else {turn = -1;}
 
-		} else {
-			turn = -1;
-		}
+		if (right >= gridSize) {right = gridSize - 1;}
+		if (left < 0) {left = 0;}
+		if (down >= gridSize) {down = gridSize - 1;}
+		if (up < 0) {up = 0;}
 
-		if (right >= gridSize) {
-			right = gridSize - 1;
-		}
-		if (left < 0) {
-			left = 0;
-		}
-		if (down >= gridSize) {
-			down = gridSize - 1;
-		}
-		if (up < 0) {
-			up = 0;
-		}
-
-		// Dernæst tjekkes om modsatte farve er rundt om feltet, hvis ja gåes videre til
-		// næste trin
+		// The actual boolean check for if enemey piece around your piece
 		if (cell[right][down].getMyValue() == turn * -1) {
 			if (line(cell, right, down, x, y)) {
 				return true;
@@ -245,9 +243,10 @@ public class Reversi extends Application {
 		return false;
 	}
 
-	// Tjekker om på den anden side af den modsat farvede brik der din farve brik.
-	// Hvis der er samme farve kaldes metoden igen.
-	// Hvis der er tomt fortsætter programmet ikke.
+	
+	//Checks the whole row, column and diagonal for enemy pieces.
+	// If your own piece --> Recursive method call
+	// If empty --> Break and return false.
 	public boolean line(MyButton[][] cell, int side_x, int side_y, int old_x, int old_y) {
 		int new_x = side_x + (side_x - old_x);
 		int new_y = side_y + (side_y - old_y);
@@ -276,7 +275,7 @@ public class Reversi extends Application {
 
 } // End class
 
-// En klasse så en knap kan tilegnes en int værdi.
+// A class that makes it possible to assign a INT value to each button
 class MyButton extends Button {
 
 	private int MyValue;
@@ -285,10 +284,12 @@ class MyButton extends Button {
 		this.MyValue = MyValue;
 	}
 
+	// A method to get the value of a button
 	public int getMyValue() {
 		return MyValue;
 	}
 
+	// A method to change the value of a button
 	public void setMyValue(int MyValue) {
 		this.MyValue = MyValue;
 	}
