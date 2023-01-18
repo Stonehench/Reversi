11af189738776cc0923c7099ba6b55ef90829cfa
michmai <121814049+michmai@users.noreply.github.com>
2023-01-18 19:09:39 +0100

    Update Winner.java

--- Reversi_med_klasser/Winner.java
+++ Reversi_med_klasser/Winner.java
@@ -1,4 +1,4 @@
-package JanuarProject;
+package Demonstration;
 
 import javafx.application.Application;
 import javafx.geometry.Pos;
@@ -12,6 +12,10 @@ import javafx.stage.Stage;
 
 public class Winner extends Application{
 
+    public static Button backToMenu2 = new Button();
+
+    
+    
     @Override
     public void start(Stage winStage) {
         
@@ -22,18 +26,29 @@ public class Winner extends Application{
 
         VBox layout = new VBox(10);
         layout.setAlignment(Pos.CENTER);
-
-        Font font = new Font("Impact", 20);
-
-        Label label = new Label("THE WINNER IS " + Model.winner + " !!!!!");
-        label.setFont(font);
+        Label label = new Label("GAME OVER");
+        Font font = new Font ("Impact", 30);
         layout.getChildren().add(label);
+        label.setFont(font);
+        
+        Button winner = new Button();
+        winner.setText("    " + Model.winner + " WINS   ");
+        winner.setOnAction(e -> winStage.close());        
+        winner.setStyle("-fx-background-radius: 15");
+        layout.getChildren().add(winner);
+        Font font1 = new Font("Arial Blac", 18);
+        winner.setFont(font1);
+        winner.setStyle("-fx-background-color: linear-gradient(to bottom, gold, goldenrod);");
+        
 
         Button closeButton = new Button();
-        closeButton.setText("Start a New Game");
+        closeButton.setText("    NEW GAME    ");
         closeButton.setOnAction(e -> winStage.close());
         closeButton.setStyle("-fx-background-radius: 15");
         layout.getChildren().add(closeButton);
+        Font font11 = new Font("Rockwell", 18);
+        closeButton.setFont(font11);
+        closeButton.setStyle("-fx-background-color: linear-gradient(to bottom, gold, goldenrod);");
 
         closeButton.setOnAction(event -> {
             if (Model.winner.equals("BLACK")) {
@@ -44,8 +59,8 @@ public class Winner extends Application{
             Model.restartGame(View.buttons2D);
             winStage.close();
         });
-
-        Scene scene = new Scene(layout);
+        
+        Scene scene = new Scene(layout,300, 200);
         winStage.setScene(scene);
         winStage.show();
         layout.setStyle("-fx-background-color: #33CC66;");
